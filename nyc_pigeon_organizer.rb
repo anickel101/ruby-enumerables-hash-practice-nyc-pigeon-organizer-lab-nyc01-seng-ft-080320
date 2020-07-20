eafa5b26f163c9a38adb88252787bcf9347ca5bc
Anson Nickel <anson.nickel@gmail.com>
2020-07-20 20:33:37 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -24,6 +24,8 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), name_hash|
     value.each do |inner_key, names|
       names.each do |name|
+      end
+    end
   end
 end
 
